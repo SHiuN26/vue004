dd5913b26a49d78ffe51fead26ca1df04161a8ff
SHiuN26 <justodme26@gmail.com>
2021-12-15 13:19:33 +0800

    1

--- vue004.js
+++ vue004.js
@@ -1,3 +1,19 @@
+var bookdata =[
+    {
+        name: "惡搞也能出頭天：Action！網路短劇天團這群人TGOP的幕後奇想、技藝、創作實驗室",
+        author: "這群人TGOP",
+        publish_house: "尖端",
+        img: "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/072/88/0010728840.jpg&v=57cfec46&w=348&h=348",
+        price: 360,
+        is_discount: true,
+        discount: 0.79,
+        link_book: "https://www.books.com.tw/products/0010728840?loc=P_003_001",
+        link_author: "https://search.books.com.tw/exep/prod_search.php?key=%E9%80%99%E7%BE%A4%E4%BA%BATGOP&f=author",
+        date: "2016/03/30"
+
+    }
+
+];
 $(document).ready(function () {
     
 });
